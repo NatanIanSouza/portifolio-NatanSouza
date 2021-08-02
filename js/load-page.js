48fd45a3ad57bad23f10d8ec69961b83b16c6acf
NatanIanSouza <iannatan4@gmail.com>
2021-08-02 10:51:35 -0300

    Commit - page

---
+++ js/load-page.js
@@ -0,0 +1,15 @@
+//Leitura completa do nosso documento HTML
+$(document).ready(function(){
+
+    //Monitorar todos os cliques em cima do elemento "a" do nosso documento HTML
+
+    $('a').click(function(e){
+        e.preventDefault()
+
+        //Criar uma variável local que receba o atributo href do link
+        let page = $(this).attr('href')
+
+        alert(page)
+    
+    })
+})

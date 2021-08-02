cc95dc9d028db726fff78c4ca84b056c52de3695
NatanIanSouza <iannatan4@gmail.com>
2021-08-02 11:24:42 -0300

    commit - Modal

--- js/load-page.js
+++ js/load-page.js
@@ -9,10 +9,18 @@ $(document).ready(function(){
         //Criar uma variável local que receba o atributo href do link
         let page = $(this).attr('href')
 
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
         //Verificar qual conteúdo eu devo carregar
         switch(page){
             //Se for para abria a página sobre mim...
-            case 'About': alert('Sobre mim'); 
+            case 'About': 
+                $('.modal-title').append('Sobre mim')
+                $('.modal-body').append(`<p class="text-danger" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates temporibus at deleniti nam dolorem voluptatem perspiciatis nostrum ipsa iusto? Perspiciatis vel deserunt error dolores reiciendis cumque necessitatibus? Modi, molestias?</p>
+                <p class="text-primary" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates temporibus at deleniti nam dolorem voluptatem perspiciatis nostrum ipsa iusto? Perspiciatis vel deserunt error dolores reiciendis cumque necessitatibus? Modi, molestias?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates temporibus at deleniti nam dolorem voluptatem perspiciatis nostrum ipsa iusto? Perspiciatis vel deserunt error dolores reiciendis cumque necessitatibus? Modi, molestias?</p>`)
+                $('#modal-info').modal('show')
             break
             //Se for para abria a página sobre mim...
             case 'Works': alert('Meus trabalhos'); 
